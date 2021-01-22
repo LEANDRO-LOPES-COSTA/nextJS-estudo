096d047bafdad81e6a9570c0744b16b154620420
Leandro-Costa <leandrolopescosta39@gmail.com>
2021-01-21 22:40:28 -0300

    teste link 4

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+
 
 function Home () {
     return (
         <div> 
             <h4> Home </h4> 
-            <Link>          
+                     
                 <a href="/sobre">Acessar página Sobre</a>  
-            </Link>           
+                     
         </div>
-        
+
     )
 }
 
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,14 +1,13 @@
-import Link from 'next/link';
 
 function Sobre () {
     return (
         <div> 
             <h4> Sobre </h4>  
-            <Link>        
+                   
                 <a href="/">Acessar página Home</a> 
-            </Link>             
+                      
         </div>
-        
+
     )
 }
 
