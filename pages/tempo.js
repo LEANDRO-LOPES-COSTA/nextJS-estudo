e609484ef01e22c7a64a12f062337fdd5c446521
Leandro-Costa <leandrolopescosta39@gmail.com>
2021-01-21 23:50:51 -0300

    tempo com delay

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,17 +1,22 @@
 function Tempo (props){
+    console.log('passando pelo Frontend');
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
             
         </div>
     )
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+    console.log('Passando pelo getStatic Props()');
+    console.log('Adicionando delay de 5 segundos');
+
+    await delay(5000);
     const dynamicDate = new Date();
     const staticDateString = dynamicDate.toGMTString();
 
@@ -21,5 +26,7 @@ export function getStaticProps() {
         }
     }
 }
+
+const delay = ms => new Promise(resolve => setTimeout (resolve, ms));
 
 export default Tempo;
